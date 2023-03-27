c138173f404ebdb45b6563f3fe43b8240ee825ee
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2023-03-28 00:24:06 +0300

    add docs

--- src/ClothesStore/ErrorOrSuccesWinController.java
+++ src/ClothesStore/ErrorOrSuccesWinController.java
@@ -1,22 +1,13 @@
 package ClothesStore;
 
-import java.io.IOException;
-import java.sql.SQLException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.FlowPane;
-import javafx.stage.Modality;
+
 import javafx.stage.Stage;
-import javafx.stage.Window;
+
 
 public class ErrorOrSuccesWinController {
 
